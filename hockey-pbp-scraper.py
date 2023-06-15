81c6e9aa2cdf1a8f2d0b7d370e809cab3be6ee35
cjuberaUNCC <cjubera@uncc.edu>
2023-06-15 09:21:49 -0400

    update scraper and start modeling

--- hockey-pbp-scraper.py
+++ hockey-pbp-scraper.py
@@ -5,62 +5,21 @@ from datetime import datetime
 import asyncio
 import aiohttp
 import requests
-import json
+import numpy as np
+from selectolax.parser import HTMLParser
 
-
-async def fetch_data(session, url):
-    async with session.get(url) as response:
-        return await response.json()
-
-async def get_game_json(game_id):
-    shifts_url = f"https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId={game_id}"
+def get_game_json(game_id):
     game_url = f"https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live?site=en_nhl"
-
-    async with aiohttp.ClientSession() as session:
-        shifts_task = asyncio.create_task(fetch_data(session, shifts_url))
-        game_task = asyncio.create_task(fetch_data(session, game_url))
-
-        shifts, game = await asyncio.gather(shifts_task, game_task)
-
-    return shifts, game
+    with requests.Session() as session:
+        game = session.get(game_url).json()
+    return  game
 
 def get_sec(time_str):
     """Get seconds from time."""
     time_obj = datetime.strptime(time_str, '%M:%S')
     return time_obj.minute * 60 + time_obj.second
 
-def get_shift_df(shifts_json):
-    shifts_array = [
-        [
-            f"{shift['firstName']} {shift['lastName']}",
-            shift['period'],
-            get_sec(shift['startTime']),
-            get_sec(shift['endTime']),
-            shift['teamAbbrev']
-        ]
-        for shift in shifts_json['data']
-    ]
-
-    return pd.DataFrame.from_records(shifts_array, columns=['player', 'period', 'shift_start_seconds', 'shift_end_seconds', 'team'])
-
-def get_players_on_ice(shifts_df, period, time_of_event, team, goalies_in_game):
-    on_ice = shifts_df[
-        (shifts_df['period'] == period) &
-        (shifts_df['shift_start_seconds'] <= time_of_event) &
-        (shifts_df['shift_end_seconds'] > time_of_event) &
-        (shifts_df['team'] == team)
-    ]
-    goalie = on_ice.loc[on_ice['player'].isin(goalies_in_game), 'player'].iloc[0] if not on_ice.empty and any(on_ice['player'].isin(goalies_in_game)) else None
-    skaters = on_ice[~on_ice['player'].isin(goalies_in_game)]['player'].values
-    
-    return goalie, skaters
-
-def get_game_goalies(game_json):            
-    return [
-        player['fullName']
-        for player in game_json['gameData']['players'].values()
-        if "primaryPosition" in player.keys() and player['primaryPosition']['code'] == 'G'  
-    ]
+import numpy as np
 
 def json_to_array(shifts_df, game_json):
     play_by_play_events = []
@@ -70,8 +29,11 @@ def json_to_array(shifts_df, game_json):
     game_date = game_json['gameData']['datetime']['dateTime']
     home_team = game_json['gameData']['teams']['home']['abbreviation']
     away_team = game_json['gameData']['teams']['away']['abbreviation']
-    goalies_in_game = get_game_goalies(game_json=game_json)
-    
+
+    # Convert the DataFrame columns to NumPy arrays for faster access
+    periods = shifts_df['Period'].values
+    times = shifts_df['Time'].values
+
     for play in game_json['liveData']['plays']['allPlays']:
         event_type = play['result']['eventTypeId']
         if event_type not in ['FACEOFF', 'HIT', 'BLOCKED_SHOT', 'MISSED_SHOT', 'GIVEAWAY', 'TAKEAWAY', 'GOAL', 'SHOT']:
@@ -79,47 +41,52 @@ def json_to_array(shifts_df, game_json):
         event_index = play['about']['eventIdx']
         game_period = play['about']['period']
         game_seconds = get_sec(play['about']['periodTime'])
-        
+
         event_description = play['result']['description']
-        
+        empty_net = play['result']['emptyNet'] if "emptyNet" in play['result'] else None
+
         event_detail = play['result'].get('secondaryType')
         event_team = play.get('team', {}).get('triCode')
-        
+
         event_players = play.get('players', [])
         event_player_1 = event_players[0]['player']['fullName'] if event_players else None
         event_player_2 = event_players[1]['player']['fullName'] if len(event_players) >= 2 else None
         event_player_3 = event_players[2]['player']['fullName'] if len(event_players) >= 3 else None
-        
+
         coords = play['coordinates']
         coords_x = coords['x'] if 'x' in coords else None
         coords_y = coords['y'] if 'y' in coords else None
-
-        home_goalie, home_skaters_on_ice = get_players_on_ice(shifts_df, game_period, game_seconds, home_team, goalies_in_game)
-        away_goalie, away_skaters_on_ice = get_players_on_ice(shifts_df, game_period, game_seconds, away_team, goalies_in_game)
-        home_skaters = len(home_skaters_on_ice)
-        away_skaters = len(away_skaters_on_ice)
-
-        home_skater_1 = home_skaters_on_ice[0] if home_skaters >= 1 else None
-        home_skater_2 = home_skaters_on_ice[1] if home_skaters >= 2 else None
-        home_skater_3 = home_skaters_on_ice[2] if home_skaters >= 3 else None
-        home_skater_4 = home_skaters_on_ice[3] if home_skaters >= 4 else None
-        home_skater_5 = home_skaters_on_ice[4] if home_skaters >= 5 else None
-        home_skater_6 = home_skaters_on_ice[5] if home_skaters >= 6 else None
-        away_skater_1 = away_skaters_on_ice[0] if away_skaters >= 1 else None
-        away_skater_2 = away_skaters_on_ice[1] if away_skaters >= 2 else None
-        away_skater_3 = away_skaters_on_ice[2] if away_skaters >= 3 else None
-        away_skater_4 = away_skaters_on_ice[3] if away_skaters >= 4 else None
-        away_skater_5 = away_skaters_on_ice[4] if away_skaters >= 5 else None
-        away_skater_6 = away_skaters_on_ice[5] if away_skaters >= 6 else None
-
         home_goals = play['about']['goals']['home']
         away_goals = play['about']['goals']['away']
 
+        # Use NumPy indexing to retrieve the values from the arrays
+        mask = (periods == str(game_period)) & (times == int(game_seconds))
+        homeskater_1 = shifts_df[mask]['Home1'].values[0] if not shifts_df[mask].empty else None
+        homeskater_2 = shifts_df[mask]['Home2'].values[0] if not shifts_df[mask].empty else None
+        homeskater_3 = shifts_df[mask]['Home3'].values[0] if not shifts_df[mask].empty else None
+        homeskater_4 = shifts_df[mask]['Home4'].values[0] if not shifts_df[mask].empty else None
+        homeskater_5 = shifts_df[mask]['Home5'].values[0] if not shifts_df[mask].empty else None
+        homeskater_6 = shifts_df[mask]['Home6'].values[0] if not shifts_df[mask].empty else None
+
+        awayskater_1 = shifts_df[mask]['Away1'].values[0] if not shifts_df[mask].empty else None
+        awayskater_2 = shifts_df[mask]['Away2'].values[0] if not shifts_df[mask].empty else None
+        awayskater_3 = shifts_df[mask]['Away3'].values[0] if not shifts_df[mask].empty else None
+        awayskater_4 = shifts_df[mask]['Away4'].values[0] if not shifts_df[mask].empty else None
+        awayskater_5 = shifts_df[mask]['Away5'].values[0] if not shifts_df[mask].empty else None
+        awayskater_6 = shifts_df[mask]['Away6'].values[0] if not shifts_df[mask].empty else None
+
+        home_goalie = shifts_df[mask]['HomeGoalie'].values[0] if not shifts_df[mask].empty else None
+        away_goalie = shifts_df[mask]['AwayGoalie'].values[0] if not shifts_df[mask].empty else None
+
+        long_description = shifts_df[mask]['Description'].values[0] if not shifts_df[mask].empty else None
+        strength = shifts_df[mask]['Strength'].values[0] if not shifts_df[mask].empty else None
+
         play_by_play_events.append([season, game_id, game_date, event_index, game_period, game_seconds, event_type, event_description, event_detail, event_team, event_player_1, event_player_2,
-                                    event_player_3, coords_x, coords_y, home_team, away_team, home_skater_1, home_skater_2, home_skater_3, home_skater_4, home_skater_5, home_skater_6, away_skater_1,
-                                    away_skater_2, away_skater_3, away_skater_4, away_skater_5, away_skater_6, home_goalie, away_goalie, home_skaters, away_skaters, home_goals, away_goals])
+                                    event_player_3, coords_x, coords_y, home_team, away_team, home_goals, away_goals, empty_net, homeskater_1, homeskater_2, homeskater_3, homeskater_4, homeskater_5,
+                                    homeskater_6, awayskater_1, awayskater_2, awayskater_3, awayskater_4, awayskater_5, awayskater_6, home_goalie, away_goalie, long_description])
 
     return play_by_play_events
+
 
 def get_game_ids(start_date, end_date):
     # Season year is the first year in the season
@@ -127,28 +94,82 @@ def get_game_ids(start_date, end_date):
     game_ids = [game['gamePk'] for day in schedule['dates'] for game in day['games']]
     return game_ids
     
+import requests
+import pandas as pd
+
+def get_shift_data(game_id):
+    id_str = str(game_id)
+    season_start_year = id_str[:4]
+    link = f"https://www.nhl.com/scores/htmlreports/{season_start_year}{int(season_start_year) + 1}/PL{id_str[4:]}.HTM"
+
+    session = requests.Session()
+    response = session.get(link)
+    html = HTMLParser(response.text)
+
+    text_elements = html.css(".bborder")
+    data = {'Column': text_elements}
+    df = pd.DataFrame(data)
+
+    df = df.groupby(df.index // 8).apply(lambda x: pd.Series(x['Column'].values))
+    df.columns = ['Index', 'Period', "Strength", "Time", "Event", "Description", "AwayOnIce", "HomeOnIce"]
+
+    df['Index'] = df['Index'].apply(lambda x: x.text())
+    df['Period'] = df['Period'].apply(lambda x: x.text())
+    df['Strength'] = df['Strength'].apply(lambda x: x.text())
+
+    time_text = df['Time'].apply(lambda x: x.text(separator='\n').split("\n")[0])
+    time_text = time_text[time_text != 'Time:']  # Exclude strings equal to 'Time:'
+    df['Time'] = pd.to_numeric(time_text.apply(get_sec), errors='coerce')
+
+    df['Event'] = df['Event'].apply(lambda x: x.text())
+    df['Description'] = df['Description'].apply(lambda x: x.text())
+
+    df['HomeOnIce'] = df['HomeOnIce'].apply(lambda x: x.css("font[style='cursor:hand;']"))
+    df['AwayOnIce'] = df['AwayOnIce'].apply(lambda x: x.css("font[style='cursor:hand;']"))
+
+    df['Home1'] = [x[0].attributes['title'].split("- ")[-1] if len(x) > 0 and "Goalie" not in x[0].attributes['title'] else None for x in df['HomeOnIce']]
+    df['Home2'] = [x[1].attributes['title'].split("- ")[-1] if len(x) > 1 and "Goalie" not in x[1].attributes['title'] else None for x in df['HomeOnIce']]
+    df['Home3'] = [x[2].attributes['title'].split("- ")[-1] if len(x) > 2 and "Goalie" not in x[2].attributes['title'] else None for x in df['HomeOnIce']]
+    df['Home4'] = [x[3].attributes['title'].split("- ")[-1] if len(x) > 3 and "Goalie" not in x[3].attributes['title'] else None for x in df['HomeOnIce']]
+    df['Home5'] = [x[4].attributes['title'].split("- ")[-1] if len(x) > 4 and "Goalie" not in x[4].attributes['title'] else None for x in df['HomeOnIce']]
+    df['Home6'] = [x[5].attributes['title'].split("- ")[-1] if len(x) > 5 and "Goalie" not in x[5].attributes['title'] else None for x in df['HomeOnIce']]
+
+    df['Away1'] = [x[0].attributes['title'].split("- ")[-1] if len(x) > 0 and "Goalie" not in x[0].attributes['title'] else None for x in df['AwayOnIce']]
+    df['Away2'] = [x[1].attributes['title'].split("- ")[-1] if len(x) > 1 and "Goalie" not in x[1].attributes['title'] else None for x in df['AwayOnIce']]
+    df['Away3'] = [x[2].attributes['title'].split("- ")[-1] if len(x) > 2 and "Goalie" not in x[2].attributes['title'] else None for x in df['AwayOnIce']]
+    df['Away4'] = [x[3].attributes['title'].split("- ")[-1] if len(x) > 3 and "Goalie" not in x[3].attributes['title'] else None for x in df['AwayOnIce']]
+    df['Away5'] = [x[4].attributes['title'].split("- ")[-1] if len(x) > 4 and "Goalie" not in x[4].attributes['title'] else None for x in df['AwayOnIce']]
+    df['Away6'] = [x[5].attributes['title'].split("- ")[-1] if len(x) > 5 and "Goalie" not in x[5].attributes['title'] else None for x in df['AwayOnIce']]
+
+    df['HomeGoalie'] = ["".join(player.attributes['title'] if "Goalie" in player.attributes['title'] else "" for player in x).split("- ")[-1] for x in df['HomeOnIce']]
+    df['AwayGoalie'] = ["".join(player.attributes['title'] if "Goalie" in player.attributes['title'] else "" for player in x).split("- ")[-1] for x in df['AwayOnIce']]
+
+    filtered_df = df[df["Event"].isin(["FAC", "GIVE", "BLOCK", "GIVE", "HIT", "GOAL", "MISS", "SHOT", "TAKE"])]
+
+    return filtered_df
+
 
 def main():
     all_games = []
-    game_ids = get_game_ids('2021-01-13', '2023-05-28')
+    game_ids = get_game_ids('2021-10-12', '2023-05-28')
     print(len(game_ids))
     start_time = time.time()
     i = 0
     for game in game_ids:
-        shifts_json, game_json = asyncio.run(get_game_json(game)) 
-        shifts_df = get_shift_df(shifts_json)
-        all_games += json_to_array(shifts_df, game_json)
         if i % 164 == 0:
-            print("--- %s seconds ---" % (time.time() - start_time))
+            print(str(i) + " --- %s seconds ---" % (time.time() - start_time))
         i += 1
-    df = pd.DataFrame(all_games, columns=["season", "game_id", "game_date", "event_index", "game_period", "game_seconds", "event_type", 
-                                          "event_description", "event_detail", "event_team", "event_player_1", "event_player_2","event_player_3", 
-                                          "coords_x", "coords_y", "home_team", "away_team", "home_skater_1", "home_skater_2", "home_skater_3", 
-                                          "home_skater_4", "home_skater_5", "home_skater_6", "away_skater_1", "away_skater_2", "away_skater_3", 
-                                          "away_skater_4", "away_skater_5", "away_skater_6", "home_goalie", "away_goalie", "home_skaters", "away_skaters", 
-                                          "home_goals", "away_goals"])
+        if str(game)[4:6] == "02":
+            game_json = get_game_json(game)
+            shifts_df = get_shift_data(game)
+            all_games += json_to_array(shifts_df, game_json)
+        else:
+            continue
+        
+    df = pd.DataFrame(all_games, columns=['season', 'game_id', 'game_date', 'event_index', 'game_period', 'game_seconds', 'event_type', 'event_description', 'event_detail', 'event_team', 
+                                          'event_player_1', 'event_player_2', 'event_player_3', 'coords_x', 'coords_y', 'home_team', 'away_team', 'home_goals', 'away_goals', 'empty_net', 
+                                          'homeskater_1', 'homeskater_2', 'homeskater_3', 'homeskater_4', 'homeskater_5', 'homeskater_6', 'awayskater_1', 'awayskater_2', 'awayskater_3', 
+                                          'awayskater_4', 'awayskater_5', 'awayskater_6', 'home_goalie', 'away_goalie', 'long_description'])
     df.to_csv('pbp_data.csv')
-    
-    
 if __name__ == "__main__":
     main()
--- modeling.R
+++ modeling.R
@@ -1,18 +1,187 @@
 library(tidyverse)
+library(caret)
+library(pROC)
+library(tidymodels)
+library(xgboost)
+library(janitor)
 
-pbp_data <- pbp_data <- read_csv("C:/Users/charl/OneDrive/Desktop/Hockey_xG/Hockey-xG-Model/pbp_data.csv", col_select = c(2:18))
+pbp_data <- pbp_data <- read_csv("C:/Users/charl/OneDrive/Desktop/Hockey_xG/Hockey-xG-Model/pbp_data.csv", col_select = c(!1))
 
 pbp_data$game_type <- substr(pbp_data$game_id,5,6)
 
+regular_season <- pbp_data %>%
+  filter(game_type == "02",
+         game_period < 5,
+         (empty_net == FALSE | is.na(empty_net)))
+
+regular_season$home_skaters <- rowSums(!is.na(regular_season[,21:26]))
+regular_season$away_skaters <- rowSums(!is.na(regular_season[,27:32]))
+
+fenwick_events <- c("MISSED_SHOT", "GOAL", "SHOT")
 
 regular_season <- 
-  pbp_data %>%
-  filter(game_type == "02") %>% 
-  filter(game_period < 5) %>% 
-  mutate(shot = if_else(event_type %in% c("MISSED_SHOT", "BLOCKED_SHOT", "GOAL", "SHOT"), 1, 0),
-         goal = if_else(event_type == "GOAL", 1, 0)) %>%
-  mutate(time_since_prev_event = if_else(event_type == "FACEOFF", NA, game_seconds - lag(game_seconds)),
-         distance_from_prev_event = if_else(event_type == "FACEOFF", NA, sqrt((coords_x - lag(coords_x)) ^ 2 + (coords_y - lag(coords_y)) ^ 2)))
+  regular_season %>% 
+  mutate(is_home = ifelse(event_team == home_team, 1 , 0),
+         is_goal = ifelse(event_type == "GOAL", 1, 0),
+         is_shot = if_else(event_type %in% fenwick_events, 1, 0),
+         time_diff = if_else(event_type == "FACEOFF", NA, game_seconds - lag(game_seconds)),
+         dist_diff = if_else(event_type == "FACEOFF", NA, sqrt((coords_x - lag(coords_x)) ^ 2 + (coords_y - lag(coords_y)) ^ 2)),
+         dist_per_time = dist_diff / time_diff,
+         is_rebound_0_sec = ifelse(time_diff == 0 & event_type %in% fenwick_events &
+                                     event_team == lag(event_team) & 
+                                     lag(event_type) %in% fenwick_events, 1, 0),
+         is_rebound_1_sec = ifelse(time_diff == 1 & event_type %in% fenwick_events &
+                                     event_team == lag(event_team) & 
+                                     lag(event_type) %in% fenwick_events, 1, 0),
+         is_rebound_2_sec = ifelse(time_diff == 2 & event_type %in% fenwick_events &
+                                     event_team == lag(event_team) & 
+                                     lag(event_type) %in% fenwick_events, 1, 0),
+         is_rebound_3_sec = ifelse(time_diff == 3 & event_type %in% fenwick_events &
+                                     event_team == lag(event_team) & 
+                                     lag(event_type) %in% fenwick_events, 1, 0),
+         is_rush = ifelse(time_diff < 4 &
+                            lag(abs(coords_x)) < 25 &
+                            event_type %in% fenwick_events,
+                          1, 0),
+         last_event = lag(event_type),
+         last_event_team_is_home = lag(is_home),
+         last_event_same_team = if_else(last_event_team_is_home == is_home, 1, 0),
+         game_state = paste0(case_when(
+           home_skaters < 3 ~ "E",
+           home_skaters == 3 ~ "3",
+           home_skaters == 4 ~ "4",
+           home_skaters == 5 ~ "5",
+           home_skaters == 6 ~ "6",
+           home_skaters > 6 ~ "E"
+         ), "v", case_when(
+           away_skaters < 3 ~ "E",
+           away_skaters == 3 ~ "3",
+           away_skaters == 4 ~ "4",
+           away_skaters == 5 ~ "5",
+           away_skaters == 6 ~ "6",
+           away_skaters > 6 ~ "E"
+         )),
+         shooter_strength = if_else(
+           event_team == home_team,
+           case_when(game_state %in% c("5v5","4v4","3v3") ~ "Even",
+                     game_state %in% c("6v5","6v4","5v4","5v3","4v3") ~ "PowerPlay",
+                     game_state %in% c("4v5","3v5","3v4","5v6","4v6") ~ "ShortHanded",
+                     TRUE ~ "Other"),
+           case_when(game_state %in% c("5v5","4v4","3v3") ~ "Even",
+                     game_state %in% c("5v6","4v6","4v5","3v5","3v4") ~ "PowerPlay",
+                     game_state %in% c("5v4","5v3","4v3","6v5","6v4") ~ "ShortHanded",
+                     TRUE ~ "Other")
+         ) ,
+         coords_y = ifelse(coords_x < 0, -1 * coords_y, coords_y),
+         coords_x = abs(coords_x),
+         shot_angle = (asin(abs(coords_y)/sqrt((87.95 - abs(coords_x))^2 + coords_y^2))*180)/ 3.14,
+         shot_angle = ifelse(abs(coords_x) > 88, 90 + (180-(90 + shot_angle)), 
+                            shot_angle),
+         distance = sqrt((87.95 - abs(coords_x))^2 + coords_y^2),
+         skater_team = if_else(is_home == 1, home_skaters, away_skaters),
+         skater_op = if_else(is_home == 0, home_skaters, away_skaters),
+         high_danger = if_else((8 > coords_y & coords_y > -8) &
+                                 (coords_x > 89 & coords_x < 59), 1, 0),
+         prev_event_team_take = if_else(event_team == lag(event_team) & lag(event_type) == "TAKEAWAY", 1, 0),
+         prev_event_opp_give = if_else(event_team != lag(event_team) & lag(event_type) == "GIVEAWAY", 1, 0),
+         prev_event_opp_block = if_else(event_team != lag(event_team) & lag(event_type) == "BLOCKED_SHOT", 1, 0)
+         
+  )
 
-  
+
+
+fenwick_data <- regular_season %>% filter(event_type %in% fenwick_events)
+
+fenwick_data$shot_type <-  sapply(fenwick_data$long_description, function(x) strsplit(x, ",")[[1]][2])
+
+fenwick_data <- fenwick_data %>% filter(!is.na(shot_type) & !is.na(dist_diff) &
+                                          !is.na(shooter_strength) &
+                                          3<=away_skaters & away_skaters<=6 &
+                                            3<=home_skaters & home_skaters<=6)
+
+
+fenwick_data <- fenwick_data %>% filter(shooter_strength =="Even")
+
+fenwick_data$is_goal <- as.factor(fenwick_data$is_goal)
+
+
+formula_model <- is_goal ~ shot_type+time_diff+dist_diff+dist_per_time+is_rush+last_event+coords_x+coords_y+
+  skater_team+skater_op+last_event_same_team+is_rebound_0_sec+is_rebound_1_sec+is_rebound_2_sec+
+  is_rebound_3_sec+prev_event_team_take+prev_event_opp_give+prev_event_opp_block+game_period+game_seconds
+
+# fenwick_data_class_bal <- ROSE::ovun.sample(formula_model, data = fenwick_data, method = "both", seed = 123, p=.08)
+# fenwick_data_class_bal <- fenwick_data_class_bal$data
+
+set.seed(1234)
+
+
+data_split <- initial_split(fenwick_data, prop = 0.7, strata = is_goal)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+
+recipe <- recipe(formula_model,
+                 data = train_data) %>%
+  step_dummy(all_nominal_predictors()) %>% 
+  step_zv(all_numeric()) %>% 
+  step_normalize(all_numeric(), -all_outcomes()) %>% 
+  prep()
+
+recipe
+
+xg_model <- boost_tree() %>%
+  set_engine("xgboost") %>% 
+  set_mode("classification")
+
+set.seed(123)
+folds <- vfold_cv(juice(recipe), strata = is_goal)
+
+
+set.seed(234)
+xg_res <- xg_model %>% 
+  fit_resamples(is_goal ~ .,
+                folds,
+                control = control_resamples(save_pred = TRUE))
+
+xg_res %>% collect_metrics()
+
+workflow <- workflow() %>%
+  add_recipe(recipe) %>%
+  add_model(xg_model)
+
+model_fit <- workflow %>% 
+  fit(train_data)
+
+model_fit
+
+test_data$xG <- predict(model_fit, new_data = test_data, type = "prob")$.pred_1
+
+g <- roc(is_goal ~ xG, data = test_data)
+plot(g)
+auc(g)
+
+fenwick_data$xG <- predict(model_fit, new_data = fenwick_data, type = "prob")$.pred_1
+full_data <- fenwick_data %>%  mutate(is_goal = as.numeric(levels(is_goal))[is_goal])
+avg_xG_by_coord <- full_data %>% group_by(coords_x, coords_y) %>%
+  summarise(xg = mean(xG))
+
+ggplot(avg_xG_by_coord, aes(coords_x, coords_y, fill = xg)) + geom_raster() +
+  scale_fill_gradient(low = 'blue', high = 'red')+
+  geom_vline(xintercept = 0, color = 'red') +
+  geom_vline(xintercept = 30, color = 'blue') +
+  geom_vline(xintercept = 90, color = 'red') +
+  xlab('X Coordinates') + ylab('Y Coordinates') +
+  labs(title = 'Average xG Value by Coordinate')
+
+xg_player <- full_data %>%
+  group_by(event_player_1, event_team) %>%
+  summarise( xG = sum(xG), Goals = sum(is_goal), Difference = sum(xG) - sum(is_goal))
+arrange(xg_player, desc(xG))
+
+
+xg_team <- full_data %>%
+  group_by(event_team) %>%
+  summarise( xG = sum(xG), Goals = sum(is_goal), Difference = sum(xG) - sum(is_goal))
+
+arrange(xg_team, desc(xG))
 
